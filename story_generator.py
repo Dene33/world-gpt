5bdc2f6f5da2ada7e9fcbed86539b76849b03046
Dene33 <den33.info@gmail.com>
2023-06-13 20:56:22 +0300

    main script for pyscript, license

---
+++ story_generator.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from shiny._main import main
+import platform
+
+path = os.path.dirname(os.path.abspath(__file__))
+apath = os.path.join(path, "app.py")
+
+if platform.system() == "Windows":
+    drive, apath = os.path.splitdrive(apath)
+    apath = apath.replace("\\", "/")
+
+sys.argv = ["shiny", "run", "--port", "37955", apath]
+main()
